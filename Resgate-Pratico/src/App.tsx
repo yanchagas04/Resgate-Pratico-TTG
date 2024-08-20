3258eb14f475df230e861f5190701f29a42e097a
Yan Campêlo das Chagas <yanchagas2004@gmail.com>
2024-08-20 20:21:40 -0300

    v0.1.0
    Base do Front-End

---
+++ Resgate-Pratico/src/App.tsx
@@ -0,0 +1,13 @@
+import './Styles/main.css'
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
