6a1364ec02a345ec538c9e3691de70e17cd3d038
Pedro Lima <121100935+PPedrinho@users.noreply.github.com>
2024-09-03 15:26:34 -0300

    header v1.0

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,11 +1,26 @@
 import './Styles/main.css'
 
+import logoPrincipal from './assets/logoVector.png'; 
+import imgPrincipal from './assets/principal.png';
+
 function App() {
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div className='Header'>
+        <div className="h-[80px] px-[25px] w-full  bg-[#a42323] justify-center items-center gap-[50px] inline-flex">
+        <img className="w-12 h-12" src={logoPrincipal} />
+        <ul className='inline-flex'>
+          <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">HOME</a></li>
+          <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">SOBRE</a></li>
+          <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">CAPACITAÇÕES</a></li>
+          <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">SERVIÇOS</a></li>
+          <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">EBOOK</a></li>
+          <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">CONTATO</a></li>
+        </ul>
+        </div>
+      </div>
+      <img className="w-full h-full" src={imgPrincipal} alt="CENTRO DE TREINAMENTO" />
+      <div>test</div>
     </>
   )
 }
