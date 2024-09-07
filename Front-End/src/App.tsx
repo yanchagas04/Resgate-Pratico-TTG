804ae27d8b14e94504239e7b96646c6e848d39f4
yanchagas04 <yanchagas2004@gmail.com>
2024-09-07 20:13:27 -0300

    v0.2.2

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,12 +1,10 @@
 import './Styles/main.css'
 import Home from './Pages/Home'
-import Teste from './Pages/Teste'
 
 function App() {
   return (
     <>
       <Home />
-      <Teste/>
     </>
   )
 }
