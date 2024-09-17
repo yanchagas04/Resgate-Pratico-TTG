d2698acf9406886a93f1eb71c05fe6307e7346a7
Pedro Lima <121100935+PPedrinho@users.noreply.github.com>
2024-09-16 21:26:23 -0300

    att 1.1

--- Front-End/src/Elements/Header.tsx
+++ Front-End/src/Elements/Header.tsx
@@ -2,18 +2,31 @@ import logoPrincipal from '../assets/LogoVector.png';
 
 function Header() {
     return (
-        <header className='Header'>
-            <div className="h-[80px] px-[25px] w-full  bg-[#a42323] justify-center items-center gap-[50px] inline-flex">
-            <img className="w-12 h-12" src={logoPrincipal} />
-            <ul className='inline-flex'>
-                <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">HOME</a></li>
-                <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">SOBRE</a></li>
-                <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">CAPACITAÇÕES</a></li>
-                <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">SERVIÇOS</a></li>
-                <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">EBOOK</a></li>
-                <li className= "  m-7 text-center text-white text-xl font-semibold font-['Inter'] "><a href="#">CONTATO</a></li>
-            </ul>
-            </div>
+        <header className="Header">
+        <div className="h-auto px-5 w-full bg-[#a42323] flex flex-col md:flex-row justify-between items-center gap-4 md:gap-[50px] py-4">
+        <img className="w-12 h-12" src={logoPrincipal} alt="Logo" />
+
+        <ul className="flex flex-col md:flex-row justify-center items-center gap-4 md:gap-6">
+          <li className="text-center text-white text-lg md:text-xl font-semibold font-['Inter']">
+            <a href="#">HOME</a>
+          </li>
+          <li className="text-center text-white text-lg md:text-xl font-semibold font-['Inter']">
+            <a href="#">SOBRE</a>
+          </li>
+          <li className="text-center text-white text-lg md:text-xl font-semibold font-['Inter']">
+            <a href="#">CAPACITAÇÕES</a>
+          </li>
+          <li className="text-center text-white text-lg md:text-xl font-semibold font-['Inter']">
+            <a href="#">SERVIÇOS</a>
+          </li>
+          <li className="text-center text-white text-lg md:text-xl font-semibold font-['Inter']">
+            <a href="#">EBOOK</a>
+          </li>
+          <li className="text-center text-white text-lg md:text-xl font-semibold font-['Inter']">
+            <a href="#">CONTATO</a>
+          </li>
+        </ul>
+        </div>
         </header>
     )
 }
