7e7acc07dd2e181aa8080130bc8251f6f4c769e4
yanchagas04 <yanchagas2004@gmail.com>
2024-09-18 01:46:27 -0300

    v0.2.2 - Criando um endpoint teste

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -4,3 +4,6 @@ var PORT = 3000;
 app.listen(PORT, function () {
     console.log("Server is running on port ".concat(PORT));
 });
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -5,3 +5,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
