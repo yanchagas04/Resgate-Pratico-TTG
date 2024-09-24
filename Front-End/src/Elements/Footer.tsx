9ca12f88c02fd83ddae30ba3a50f60bb9c76cfd2
Pedro Lima <121100935+PPedrinho@users.noreply.github.com>
2024-09-24 16:05:00 -0300

    footer 2.0

--- Front-End/src/Elements/Footer.tsx
+++ Front-End/src/Elements/Footer.tsx
@@ -5,50 +5,80 @@ import LogoLogo from '../assets/logoVector.png';
 
 function Footer() { 
     return (
+        
+<footer className="w-full font-['Inter'] bg-neutral-800 p-6">
+     {/* Container principal centralizado */}
+  <div className="max-w-7xl mx-auto flex flex-col justify-center items-center text-white">
+    
+    {/* Seção de Redes Sociais */}
+    <div className="flex justify-center items-center space-x-8 mb-6">
+      <div className="flex items-center gap-4">
+        <img className="w-11 h-[38px]" src={InstaLogo} alt="Instagram" />
+        <a href="https://www.instagram.com/ctresgatepraticoba/" target="_blank" className="text-lg font-bold">
+          Instagram
+        </a>
+      </div>
+      <div className="flex items-center gap-4">
+        <img className="w-[39px] h-[39px]" src={LinkLogo} alt="Linkedin" />
+        <a href="https://br.linkedin.com/company/resgate-pr%C3%A1tico" target="_blank" className="text-lg font-bold">
+          Linkedin
+        </a>
+      </div>
+      <div className="flex items-center gap-4">
+        <img className="w-6 h-9" src={FacebookLogo} alt="Facebook" />
+        <a href="https://www.facebook.com/resgatepraticoba/" target="_blank" className="text-lg font-bold">
+          Facebook
+        </a>
+      </div>
+    </div>
 
-        <div className="w-full h-full p-2.5 bg-neutral-800 justify-center md:flex-row items-center gap-2.5 md:gap-[50px] inline-flex">
-        <div className="w-[1446px] h-[454px] relative bg-neutral-800">
-            <div className="w-[1339.68px] h-[360.37px] left-[58.48px] top-[46px] absolute">
-                <img className="w-[120.46px] h-[108.67px] left-[1430.21px] top-[251.70px] absolute" src={LogoLogo} />
-                <div className="h-[39px] left-[321.52px] top-0 absolute justify-start items-center gap-[146px] inline-flex">
-                    <div className="h-[38px] justify-center items-center gap-4 flex">
-                        <img className="w-11 h-[38px]" src={InstaLogo} />
-                        <div className="w-[99px] h-[23px] text-[#fbf8f3] text-[19px] font-bold font-['Inter'] tracking-tight"><a href='https://www.instagram.com/ctresgatepraticoba/'>Instagram</a></div>
-                    </div>
-                    <div className="h-[39px] justify-center items-center gap-4 flex">
-                        <img className="w-[39px] h-[39px]" src={LinkLogo} />
-                        <div className="w-[99px] h-[23px] text-[#fbf8f3] text-[19px] font-bold font-['Inter'] tracking-tight"><a href='https://br.linkedin.com/company/resgate-pr%C3%A1tico'>Linkedin</a></div>
-                    </div>
-                    <div className="w-[139px] h-9 justify-center items-center gap-4 flex">
-                        <img className="w-6 h-9" src={FacebookLogo} />
-                        <div className="w-[99px] h-[23px] text-[#fbf8f3] text-[19px] font-bold font-['Inter'] tracking-tight"><a href='https://www.facebook.com/resgatepraticoba/'>Facebook</a></div>
-                    </div>
-                </div>
-                <div className="w-[462.51px] h-[46.04px] left-[405.09px] top-[313.79px] absolute text-[#fbf8f3] text-[19px] font-normal font-['Inter'] tracking-tight">© Resgate Prático 2024 - Direitos Reservados</div>
-                
-            </div>
-            
-            <div className="w-[411.47px] h-[104.98px] left-[489.09px] top-[127.72px] absolute flex-col justify-start items-start gap-5 inline-flex">
-                <div className="self-stretch h-[23px] text-white text-[19px] font-bold font-['Inter'] tracking-tight"><ul>Endereço:</ul><br/></div>
-                <div className="self-stretch h-[71px] text-white text-[19px] font-bold font-['Inter'] underline tracking-tight"><li>Rua Francisca Garcez Montenegro - Montenegro - Camaçari, BA, 42812-180</li></div>
-            </div>
-            <div className="w-[280.69px] h-[107.74px] left-[1007.95px] top-[124.96px] absolute">
-                <div className="w-[90.38px] h-[21.18px] left-0 top-0 absolute text-white text-[19px] font-bold font-['Inter'] tracking-tight"><ul>Contato:</ul><br/></div>
-                <div className="w-[278.57px] h-[21.18px] left-[0.01px] top-[86.56px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li>resgatepratico@gmail.com</li></div>
-                <div className="w-[177.56px] h-[21.18px] left-[-0px] top-[41.44px] absolute text-white text-[19px] font-bold font-['Inter'] underline tracking-tight"><li>(71) 3016-1787</li></div>
-            </div>
-            <div className="w-[149px] h-[245px] left-[100px] top-[133px] absolute">
+    {/* Linha divisória */}
+    <div className="w-full border-t border-white my-4"></div>
 
-                <div className="w-[140px] h-[23px] left-0 top-0 absolute text-white text-[19px] font-bold font-['Inter'] tracking-tight"><ul>Menu:</ul></div>
-                <div className="w-[140px] h-[23px] left-0 top-[38px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li><a href="#">Home</a></li></div>
-                <div className="w-[140px] h-[23px] left-0 top-[76px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li><a href="#">Sobre</a></li></div>
-                <div className="w-40 h-[23px] left-0 top-[114px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li><a href="#">Capacitações</a></li></div>
-                <div className="w-[140px] h-[23px] left-0 top-[152px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li><a href="#">Serviços</a></li></div>
-                <div className="w-[140px] h-[23px] left-[0.06px] top-[189.75px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li><a href="#">E-book</a></li></div>
-                <div className="w-[140px] h-[23px] left-0 top-[222px] absolute text-white text-[19px] font-normal font-['Inter'] tracking-tight"><li><a href="#">Contato</a></li></div>
-            </div>
-        </div>
-    </div>       
+    {/* Seções de Menu, Endereço, Contato */}
+    <div className="w-full flex flex-col md:flex-row justify-around items-start mb-6 space-y-4 md:space-y-0">
+      
+      {/* Menu */}
+      <div className="text-left">
+        <h4 className="text-lg font-bold mb-2">Menu:</h4>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Capacitações</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">E-book</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </div>
+
+      {/* Endereço */}
+      <div className="text-left">
+        <h4 className="text-lg font-bold mb-2">Endereço:</h4>
+        <ul>
+          <li className="underline">Rua Francisca Garcez Montenegro<br/>Montenegro - Camaçari, BA, 42812-180</li>
+        </ul>
+      </div>
+
+      {/* Contato */}
+      <div className="text-left">
+        <h4 className="text-lg font-bold mb-2">Contato:</h4>
+        <ul>
+          <li className="underline font-bold">(71) 3016-1787</li>
+          <li>resgatepratico@gmail.com</li>
+        </ul>
+      </div>
+    </div>
+
+    {/* Logo e Direitos Reservados */}
+    <div className="flex flex-col items-center">
+      <img className="w-16 h-16 mb-4" src={LogoLogo} alt="Logo Resgate Prático" />
+      <p className="text-center text-sm">
+        © Resgate Prático 2024 - Direitos Reservados
+      </p>
+    </div>
+    </div>
+</footer>
+     
 
     )
 }
