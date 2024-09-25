1341e3be2d850ecb566478f2a6f398d02d3f1699
yanchagas04 <yanchagas2004@gmail.com>
2024-09-25 10:02:44 -0300

    v0.4

--- Front-End/src/Elements/CardDivisao.tsx
+++ Front-End/src/Elements/CardDivisao.tsx
@@ -9,8 +9,9 @@ interface CardDivisaoProps {
 
 function CardDivisao(props: CardDivisaoProps) {
     const [isHovered, setIsHovered] = useState(false);
+    const background = `bg-[url('${props.imagem}')]`
     return (
-        <a className={`transition-all duration-300 ease-in-out flex flex-col items-center justify-center bg-[url(${props.imagem})] bg-gray-400 hover:bg-blend-multiply w-72 h-[500px] rounded-2xl border-4 border-transparent hover:border-gray-700 p-4`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <a className={`transition-all duration-300 ease-in-out flex flex-col items-center justify-center ${background} bg-gray-400 hover:bg-blend-multiply w-72 h-[500px] rounded-2xl border-4 border-transparent hover:border-gray-700 p-4`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             <h1 className="text-center text-2xl text-white font-[Inter] font-bold">{props.nome}</h1>
             {isHovered && <p className="text-center text-sm text-white font-[Inter] animate-pulse">{props.descricao}</p>}
         </a>
