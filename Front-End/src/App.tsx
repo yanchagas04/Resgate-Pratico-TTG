4715ecaa8de9ad5d9c958b9180b36fd91b66dd5a
yanchagas04 <yanchagas2004@gmail.com>
2024-10-01 16:21:25 -0300

    v0.1 - Formulario

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -3,13 +3,13 @@ import Home from './Pages/Home'
 import PaginaCursos from './Pages/PaginaCursos'
 import PaginaServicos from './Pages/PaginaServicos'
 import Footer from './Elements/Footer'
+import FormsContato from './Pages/FormsContato'
 
 
 function App() {
   return (
     <>
-      <PaginaServicos />
-      
+      <FormsContato /> 
     </>
   )
 }
---
+++ Front-End/src/Pages/FormsContato.tsx
@@ -0,0 +1,51 @@
+import Logo from '../assets/LogoVector.png';
+import IconeTelefone from '../assets/IconeTelefone.png';
+
+function FormsContato() {
+    const cursos = [];
+    const servicos = [];
+    const equipamentos = [];
+    return (
+        <div className="flex flex-col-reverse lg:flex-row-reverse w-full min-h-screen font-[Inter] transition-all duration-150 ease-in">
+            <div className="flex w-full bg-slate-100 items-center justify-center py-8">
+                <form className='min-w-96 w-3/4 h-fit flex flex-col justify-center items-center bg-white drop-shadow-lg rounded-lg p-4 gap-4'>
+                    <img src={Logo} alt="Logotipo" className="lg:hidden w-48" />
+                    <h1 className='lg:hidden text-gray-950 text-xl'>Formulário de Contato</h1>
+                    <div className='flex flex-col gap-4 w-full'>
+                        <label htmlFor="nome" className=''>Nome:</label>
+                        <input type="text" id="nome" name="nome" className=' bg-gray-100 border border-gray-300 rounded-md p-2' />
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder='seuemail@email.com' className='bg-gray-100 border border-gray-300 rounded-md p-2' />
+                        <label htmlFor="telefon">Telefone para contato:</label>
+                        <input type="text" id="email" placeholder='(xx) 9xxxx-xxxx' name="email" className='bg-gray-100 border border-gray-300 rounded-md p-2' />
+                    </div>
+                    <div className='flex flex-col w-full'>
+                        <h3>Cursos selecionados:</h3>
+                        {cursos.length === 0 && <p className='p-2 pl-4'>Nenhum curso selecionado</p>}
+                    </div>
+                    <div className='flex flex-col w-full'>
+                        <h3>Serviços selecionados:</h3>
+                        {servicos.length === 0 && <p className='p-2 pl-4'>Nenhum serviço selecionado</p>}
+                    </div>
+                    <div className='flex flex-col w-full'>
+                        <h3>Equipamentos selecionados:</h3>
+                        {equipamentos.length === 0 && <p className='p-2 pl-4'>Nenhum equipamento selecionado</p>}
+                    </div>
+                    <button type="submit" className='bg-green-500 text-white w-full p-3 rounded-md hover:bg-green-600 transition-all duration-150 ease-in'>Enviar</button>
+                </form>
+            </div>
+            <div>
+                <nav className="hidden h-full lg:flex lg:flex-col lg:min-w-[256px] w-2/6 lg:max-w-96 border-r-2 items-center justify-center gap-4 p-4">
+                    <img src={Logo} alt="Logotipo" className="w-48" />
+                    <h1 className="text-xl text-gray-950 h-fit">Formulário de Contato</h1>
+                    <div className='flex flex-row gap-1 items-center h-fit'>
+                        <img src={IconeTelefone} alt="Ícone de Telefone" className='w-6'/>
+                        <p className='text-gray-950 h-fit'>(71)99161-7848</p>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default FormsContato;
