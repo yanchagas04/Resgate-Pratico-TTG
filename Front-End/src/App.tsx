820663dbc09f669275dcf6e313432b02e6d1f781
yanchagas04 <yanchagas2004@gmail.com>
2024-10-13 20:56:35 -0300

    v2.0 - CardCSE
    Cards de Cursos, Serviços e Equipamentos

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -4,12 +4,13 @@ import PaginaCursos from './Pages/PaginaCursos'
 import PaginaServicos from './Pages/PaginaServicos'
 import Footer from './Elements/Footer'
 import FormsContato from './Pages/FormsContato'
+import CardCSE from './Elements/CardCSE'
 
 
 function App() {
   return (
     <>
-      <FormsContato /> 
+      <Home />
     </>
   )
 }
