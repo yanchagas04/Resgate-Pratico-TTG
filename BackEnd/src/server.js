5dad208977e3b816723f71b3f43d510e903af668
Yan Campêlo das Chagas <yanchagas2004@gmail.com>
2024-10-24 13:44:37 -0300

    .

--- BackEnd/src/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var PORT = 3000;
-app.listen(PORT, function () {
-    console.log("Server is running on port ".concat(PORT));
-});
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
